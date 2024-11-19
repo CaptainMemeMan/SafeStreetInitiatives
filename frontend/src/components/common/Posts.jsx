0016c002edf0184109786aa5bb3997927c44fa20
Kirk Daniel Caponpon <caponponkirk24@gmail.com>
2024-11-19 18:58:17 -0500

    FINISHED

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,18 +3,24 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId }) => {
 	const getPostEndpoint = () => {
 		switch (feedType) {
 			case "forYou":
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
 			default:
 				return "/api/posts/all";
 		}
 	};
+
 	const POST_ENDPOINT = getPostEndpoint();
+
 	const {
 		data: posts,
 		isLoading,
@@ -26,18 +32,21 @@ const Posts = ({ feedType }) => {
 			try {
 				const res = await fetch(POST_ENDPOINT);
 				const data = await res.json();
+
 				if (!res.ok) {
 					throw new Error(data.error || "Something went wrong");
 				}
+
 				return data;
 			} catch (error) {
 				throw new Error(error);
 			}
 		},
 	});
+
 	useEffect(() => {
 		refetch();
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 
 	return (
 		<>
